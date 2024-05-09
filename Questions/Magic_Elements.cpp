31ae4da7fc991d64ea12f4011bede8f01609e63f
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-05-09 09:07:38 +0530

    Magic_Elements 1 Star DSA SHEET Deepak Sir

--- Questions/Magic_Elements.cpp
+++ Questions/Magic_Elements.cpp
@@ -1,40 +1,39 @@
-// https://www.codechef.com/problems/ZOZ
+// // https://www.codechef.com/problems/ZOZ
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
 	// your code goes here
-	int t ;
-	cin >>t;
+	int t;
+	cin >> t;
 	while (t--)
 	{
-	    int n ;
-	    cin >> n ;
-	    int k ;
-	    cin >> k ;
-	    int sum = 0;
-	    int count = 0;
-	    vector<int>v(n);
-	    for(int i = 0 ; i < n ; i++)
-	    {
-	        cin>>v[i];
-	        sum+=v[i];
-	    }
-	    for(int i = 0 ; i < n ; i++)
-	    {
-	    
-	       if( (v[i]+k) > (sum - v[i]) )
-	       count+=1;
-	    }
-	    cout<<count<<endl;
-	}
+		int n;
+		cin >> n;
+		int k;
+		cin >> k;
+		int sum = 0;
+		int count = 0;
+		vector<int> v(n);
+		for (int i = 0; i < n; i++)
+		{
+			cin >> v[i];
+			sum += v[i];
+		}
+		for (int i = 0; i < n; i++)
+		{
 
+			if ((v[i] + k) > (sum - v[i]))
+				count += 1;
+		}
+		cout << count << endl;
+	}
 }
 
-
 /*
-Input 
+Input
 
 2
 4 4
@@ -49,3 +48,4 @@ Output
 
 
 */
+
