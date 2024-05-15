280b8bc8ab7b1db8302aa8ffaa5abf7c12386173
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-05-15 21:58:45 +0530

    DIV 4 Contest

---
+++ CodeChef Contests/STARTERS 133 DIVISION 4/Devouring_Donuts.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/START133D/problems/DEVDON
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int number_of_donuts ;
+    cin>> number_of_donuts;
+    
+    int calories ;
+    cin >> calories ;
+    cout<< number_of_donuts * calories ;
+}
