c1962ff59ff4f8d6fc4b0c2d541d2e6d56edf80d
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-05-15 22:02:03 +0530

    DIV 4 Contest

---
+++ CodeChef Contests/STARTERS 134 DIVISION 4/Morning_Run.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/START134D/problems/MORNINGRUN
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int length;
+    int breadth;
+    cin>>length;
+    cin>>breadth;
+
+    if((length*2)+(breadth*2)>=1000)
+    cout<<"YES";
+    else
+    cout<<"NO";
+}
