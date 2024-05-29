abc27d062b747d8bd640c504ba3816315c87719d
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-05-29 23:40:33 +0530

    Contest

---
+++ CodeChef Contests/STARTERS 136 DIVISION 4/Who_Makes_P1.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/START136D/problems/WHOMAKESP1
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int a ;
+	int b;
+	cin >> a >> b;
+    if(a<=b)
+    cout<<"Tyro";
+    else
+    cout<<"Dom";
+}
