1a3508ede43881009569d27ab63d41efc6b8d73b
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-05-29 23:38:37 +0530

    Contest

---
+++ CodeChef Contests/STARTERS 135 DIVISION 4/RCB_vs_CSK.cpp
@@ -0,0 +1,22 @@
+// https://www.codechef.com/START135D/problems/RCBCSK
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+	// your code goes here
+    int a ;
+    int b ;
+    cin >> a;
+    cin >> b;
+
+    if(a-b>=18)
+        cout<<"RCB";
+    else
+        cout<<"CSK";
+
+
+}
