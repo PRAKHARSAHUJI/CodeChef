0d69276435a70519e7132214278139e4c17a0422
PRAKHARSAHUJI <prakharsahu1012@gmail.com>
2024-06-13 00:41:54 +0530

    Contest Div 4

---
+++ CodeChef Contests/STARTERS 138 DIVISION 4/HeatWave.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/START138C/problems/HEATWAVE
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int a ;
+    int b;
+    cin>>a>>b;
+    if(b>a)
+    cout<<"YES";
+    else
+    cout<<"NO";
+}
